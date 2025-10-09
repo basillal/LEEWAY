0738dd3f1fdb31620666c266d41285439e5857af
BASIL LAL <112262693+basillal@users.noreply.github.com>
2025-10-10 00:05:10 +0530

    Improved modal and dialog designs, (#39)
    * Resolved layout issues for small screens
    
    * Optimized state/district dropdown logic
    
    * Improved modal and dialog designs,

--- projects/core/src/app/modules/roles/roles.component.ts
+++ projects/core/src/app/modules/roles/roles.component.ts
@@ -7,18 +7,7 @@ import { InputConfig } from '@libs/dynamic-form';
   styleUrls: ['./roles.component.css']
 })
 export class RolesComponent {
-  stepColors = ['#f87171', '#fbbf24', '#34d399', '#60a5fa']; // red, yellow, green, blue
-  currentStepIndex = 0;
-  // Optional: use this if you want to go to the next step
-  goToNextStep() {
-    this.currentStepIndex++;
-  }
 
-  goToPreviousStep() {
-    if (this.currentStepIndex > 0) {
-      this.currentStepIndex--;
-    }
-  }
 
   layoutStyle: 'grid' | 'create' = 'grid';
 
